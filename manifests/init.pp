f06cc3131925bd54cfe15095806e2e2ea19133c6
Jurgen Weber <j.weber@foodpanda.com>
2014-10-12 09:22:21 +0200

    use the full names

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,9 +38,9 @@ class capistrano (
 
   #I assume the deploy host will not serve code but we will stil deploy here to
   #do 'stuff' that only one server can do.... like db migrations
-  #node { $environments: 
-    #app_name       => $app_name,
-    #primary_node   => true,
-  #}
+  capistrano::node { $environments: 
+    app_name       => $app_name,
+    primary_node   => true,
+  }
 
 }
